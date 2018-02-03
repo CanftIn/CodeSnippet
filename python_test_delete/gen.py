82b1312a51d1e230e317eca91573d29006a29cb1
CanftIn <wwc7033@gmail.com>
2018-02-03 22:46:01 +0800

    update

---
+++ python_test_delete/gen.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+for n in fib(6):
+    print(n)
