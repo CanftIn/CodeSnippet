aa1c2d6f41024cc13012d6c7b74a3c6b03148a8b
CanftIn <wwc7033@gmail.com>
2018-02-17 13:46:00 +0800

    change dir

---
+++ Haskell/haskell-tut.hs
@@ -0,0 +1,19 @@
+import Data.List
+import System.IO
+
+maxInt = maxBound :: Int
+
+minInt = minBound :: Int
+
+-- Double
+bigFloat = 3.999999999999 + 0.000000000005
+
+sumOfNums = sum[1..1000]
+
+addEx = 5 + 4
+subEx = 5 - 4
+
+-- add = '+' 5 4
+-- sub = - 3 2
+
+modEx = mod 5 4
