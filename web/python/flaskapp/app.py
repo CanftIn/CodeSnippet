1b5dd4e31fa11b71a4ec4c3173ddea6098ed0c24
CanftIn <wwc7033@gmail.com>
2018-04-10 13:08:50 +0800

    add flaskapp

---
+++ web/python/flaskapp/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+
+Articles = Articles()
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id = id)
+
+class RegisterForm(form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+# User Register
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html', form=form)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ web/python/flaskapp/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'CanftIn is a program lover.',
+            'author': 'CanftIn',
+            'create_date': '02-24-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'CanftIn is a program lover.',
+            'author': 'CanftIn',
+            'create_date': '02-24-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'CanftIn is a program lover.',
+            'author': 'CanftIn',
+            'create_date': '02-24-2018'
+        }
+    ]
+    return articles
