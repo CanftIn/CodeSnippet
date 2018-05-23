7833c8a8010398c94bdb023502b6d76a6c52f90b
CanftIn <wwc7033@gmail.com>
2018-05-23 14:34:06 +0800

    update

--- Algorithm/Interview_Of_BinaryTree_Case/Examination_Of_BinaryTree.java
+++ Algorithm/Interview_Of_BinaryTree_Case/Examination_Of_BinaryTree.java
@@ -55,5 +55,61 @@ class TreeNode_Examination
         return numOfKLevel(root.left, k - 1) + numOfKLevel(root.right, k - 1);
     }
 
-    
+    //判断是否平衡
+    boolean isBalanced(TreeNode node)
+    {
+        return maxDepth2(node) != 1;
+    }
+
+    int maxDepth2(TreeNode node)
+    {
+        if(node == null) return 0;
+        int left = maxDepth2(node.left);
+        int right = maxDepth2(node.right);
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1)
+            return -1;
+        return Math.max(left, right) + 1;
+    }
+
+    //判断树相同
+    boolean isSameTreeNode(TreeNode t1,TreeNode t2){
+        if(t1==null&&t2==null){
+            return true;
+        }
+        else if(t1==null||t2==null){
+            return false;
+        }
+        if(t1.val != t2.val){
+            return false;
+        }
+        boolean left = isSameTreeNode(t1.left,t2.left);
+        boolean right = isSameTreeNode(t1.right,t2.right);
+        return left&&right;
+    }
+
+    //判断树镜像
+    boolean isMirror(TreeNode t1,TreeNode t2){
+        if(t1==null&&t2==null){
+            return true;
+        }
+        if(t1==null||t2==null){
+            return false;
+        }
+        if(t1.val != t2.val){
+            return false;
+        }
+        return isMirror(t1.left,t2.right)&&isMirror(t1.right,t2.left);
+    }
+
+    //翻转二叉树
+    TreeNode mirrorTreeNode(TreeNode root){
+        if(root == null){
+            return null;
+        }
+        TreeNode left = mirrorTreeNode(root.left);
+        TreeNode right = mirrorTreeNode(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
 }
