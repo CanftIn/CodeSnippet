d47e7590f0aac186bcab9be5d124674ea41c6b2a
CanftIn <wwc7033@gmail.com>
2018-05-28 14:13:34 +0800

    add

--- Algorithm/Search/Binary_Search.cpp
+++ Algorithm/Search/Binary_Search.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// loop
 int binary_search(int arr[], int key)
 {
     int low = 0, mid, high = 7;
@@ -9,6 +10,7 @@ int binary_search(int arr[], int key)
 
     while(low <= high)
     {
+        //mid = low + (high - low) / 2;
         mid = (low + high) / 2;
         if(arr[mid] == key)
         {
@@ -24,6 +26,19 @@ int binary_search(int arr[], int key)
         }
     }
     return -1;
+}
+
+// recursive
+int bsearch(int arr[], int low, int high, int target)
+{
+    if(low > high) return -1;
+    int mid = (low + high) / 2;
+    if(arr[mid] > target)
+        return bsearch(arr, low, mid - 1, target);
+    if(arr[mid] < target)
+        return bsearch(arr, mid + 1, high, target);
+    
+    return mid;
 }
 
 int main()
