9b3d079275cd2acc8f2553b508f76764d982d1e8
CanftIn <wwc7033@gmail.com>
2020-03-03 17:41:56 +0800

    Update Clojure maze project

---
+++ Clojure/maze-clj/src/maze_clj/core.clj
@@ -0,0 +1,266 @@
+(ns maze-clj.core)
+
+(defn empty-board
+  "Create a rectangular empty board"
+  [w h]
+  (vec (repeat w (vec (repeat h nil)))))
+
+
+(defn populate
+  "Turn :on each of the cells specified as [y, x] coordinates."
+  [board living-cells]
+  (reduce (fn [board coordinates]
+            (assoc-in board coordinates :on))
+          board
+          living-cells))
+
+(def glider (populate (empty-board 6 6) #{[2 0] [2 1] [2 2] [1 2] [0 1]}))
+
+
+(defn neighbours
+  [[x y]]
+  (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)] 
+    [(+ dx x) (+ dy y)]))
+
+(defn count-neighbours
+  [board loc]
+  (count (filter #(get-in board %) (neighbours loc))))
+
+(defn indexed-step 
+  "Yields the next state of the board, using indices to determine neighbors,
+   liveness, etc."
+  [board]
+  (let [w (count board)
+        h (count (first board))]
+    (loop [new-board board x 0 y 0]
+      (cond
+        (>= x w) new-board
+        (>= y h) (recur new-board (inc x) 0)
+        :else
+          (let [new-liveness
+                 (case (count-neighbours board [x y])
+                   2 (get-in board [x y])
+                   3 :on
+                   nil)]
+            (recur (assoc-in new-board [x y] new-liveness) x (inc y)))))))
+
+
+(defn indexed-step2
+  [board]
+  (let [w (count board)
+        h (count (first board))]
+    (reduce 
+      (fn [new-board x]
+        (reduce 
+          (fn [new-board y]
+            (let [new-liveness
+                   (case (count-neighbours board [x y])
+                     2 (get-in board [x y])
+                     3 :on
+                     nil)]
+              (assoc-in new-board [x y] new-liveness)))
+          new-board (range h)))
+      board (range w))))
+
+
+(defn indexed-step3
+  [board]
+  (let [w (count board)
+        h (count (first board))]
+    (reduce 
+      (fn [new-board [x y]]
+        (let [new-liveness
+               (case (count-neighbours board [x y])
+                 2 (get-in board [x y])
+                 3 :on
+                 nil)]
+           (assoc-in new-board [x y] new-liveness)))
+      board (for [x (range h) y (range w)] [x y]))))
+
+
+(defn window
+  "Returns a lazy sequence of 3-item windows centered
+   around each item of coll, padded as necessary with
+   pad or nil."
+  ([coll] (window nil coll))
+  ([pad coll]
+   (partition 3 1 (concat [pad] coll [pad]))))
+
+(defn cell-block 
+  "Creates a sequences of 3x3 windows from a triple of 3 sequences."
+  [[left mid right]]
+  (window (map vector left mid right))) 
+
+(defn liveness
+  "Returns the liveness (nil or :on) of the center cell for
+   the next step."  
+  [block]
+  (let [[_ [_ center _] _] block]
+    (case (- (count (filter #{:on} (apply concat block)))
+             (if (= :on center) 1 0))
+      2 center
+      3 :on
+      nil)))
+
+(defn- step-row
+  "Yields the next state of the center row."
+  [rows-triple]
+  (vec (map liveness (cell-block rows-triple)))) 
+
+(defn index-free-step
+  "Yields the next state of the board."
+  [board]
+  (vec (map step-row (window (repeat nil) board))))
+
+(defn step 
+ "Yields the next state of the world"
+ [cells]
+ (set (for [[loc n] (frequencies (mapcat neighbours cells))
+            :when (or (= n 3) (and (= n 2) (cells loc)))]
+        loc)))
+
+(defn stepper 
+  "Returns a step function for Life-like cell automata.
+   neighbours takes a location and return a sequential collection
+   of locations. survive? and birth? are predicates on the number
+   of living neighbours."
+  [neighbours birth? survive?] 
+  (fn [cells]
+    (set (for [[loc n] (frequencies (mapcat neighbours cells))
+               :when (if (cells loc) (survive? n) (birth? n))]
+           loc))))
+
+(defn hex-neighbours
+  [[x y]]
+  (for [dx [-1 0 1] dy (if (zero? dx) [-2 2] [-1 1])] 
+    [(+ dx x) (+ dy y)]))
+
+(def hex-step (stepper hex-neighbours #{2} #{3 4}))
+
+(defn rect-stepper 
+  "Returns a step function for standard game of life on a (bounded) rectangular
+   board of specified size."
+  [w h]
+  (stepper #(filter (fn [[i j]] (and (< -1 i w) (< -1 j h))) 
+                    (neighbours %)) #{2 3} #{3}))
+
+(defn draw
+  [w h step cells]
+  (let [state (atom cells)
+        run (atom true)
+        listener (proxy [java.awt.event.WindowAdapter] []
+                   (windowClosing [_] (reset! run false)))
+        pane
+          (doto (proxy [javax.swing.JPanel] []
+                  (paintComponent [^java.awt.Graphics g]
+                    (let [g (doto ^java.awt.Graphics2D (.create g)
+                              (.setColor java.awt.Color/BLACK)
+                              (.fillRect 0 0 (* w 10) (* h 10))
+                              (.setColor java.awt.Color/WHITE))]
+                      (doseq [[x y] @state]
+                        (.fillRect g (inc (* 10 x)) (inc (* 10 y)) 8 8)))))
+            (.setPreferredSize (java.awt.Dimension. (* 10 w) (* 10 h))))] 
+    (doto (javax.swing.JFrame. "Quad Life")
+      (.setContentPane pane)
+      (.addWindowListener listener)
+      .pack
+      (.setVisible true))
+    (future (while @run
+              (Thread/sleep 80)
+              (swap! state step)
+              (.repaint pane)))))
+
+(defn rect-demo []
+  (draw 30 30 (rect-stepper 30 30) 
+        #{[15 15] [15 17] [16 16] [15 16]}))
+
+(defn maze 
+  "Returns a random maze carved out of walls; walls is a set of
+   2-item sets #{a b} where a and b are locations.
+   The returned maze is a set of the remaining walls."
+  [walls]
+  (let [paths (reduce (fn [index [a b]]
+                        (merge-with into index {a [b] b [a]}))
+                      {} (map seq walls))
+        start-loc (rand-nth (keys paths))]
+    (loop [walls walls
+           unvisited (disj (set (keys paths)) start-loc)]
+      (if-let [loc (when-let [s (seq unvisited)] (rand-nth s))]
+        (let [walk (iterate (comp rand-nth paths) loc)
+              steps (zipmap (take-while unvisited walk) (next walk))]
+          (recur (reduce disj walls (map set steps))
+                 (reduce disj unvisited (keys steps))))
+        walls))))
+
+
+(defn grid
+  [w h]
+  (set (concat
+         (for [i (range (dec w)) j (range h)] #{[i j] [(inc i) j]})
+         (for [i (range w) j (range (dec h))] #{[i j] [i (inc j)]}))))
+
+(defn draw
+  [w h maze]
+  (doto (javax.swing.JFrame. "Maze")
+    (.setContentPane 
+      (doto (proxy [javax.swing.JPanel] []
+              (paintComponent [^java.awt.Graphics g]
+                (let [g (doto ^java.awt.Graphics2D (.create g)
+                          (.scale 10 10)
+                          (.translate 1.5 1.5)
+                          (.setStroke (java.awt.BasicStroke. 0.4)))]
+                  (.drawRect g -1 -1 w h)
+                  (doseq [[[xa ya] [xb yb]] (map sort maze)]
+                    (let [[xc yc] (if (= xa xb) 
+                                    [(dec xa) ya]
+                                    [xa (dec ya)])]
+                      (.drawLine g xa ya xc yc))))))
+        (.setPreferredSize (java.awt.Dimension. 
+                       	     (* 10 (inc w)) (* 10 (inc h))))))
+    .pack
+    (.setVisible true)))
+
+
+(require '[clojure.zip :as z])
+
+
+(defn draw
+  [w h maze path]
+  (doto (javax.swing.JFrame. "Maze")
+   (.setContentPane 
+     (doto (proxy [javax.swing.JPanel] []
+             (paintComponent [^java.awt.Graphics g]
+               (let [g (doto ^java.awt.Graphics2D (.create g)
+                         (.scale 10 10)
+                         (.translate 1.5 1.5)
+                         (.setStroke (java.awt.BasicStroke. 0.4)))]
+                 (.drawRect g -1 -1 w h)
+                 (doseq [[[xa ya] [xb yb]] (map sort maze)]
+                   (let [[xc yc] (if (= xa xb) 
+                                   [(dec xa) ya]
+                                   [xa (dec ya)])]
+                     (.drawLine g xa ya xc yc)))
+                 (.translate g -0.5 -0.5)
+                 (.setColor g java.awt.Color/RED)
+                 (doseq [[[xa ya] [xb yb]] path]
+                   (.drawLine g xa ya xb yb)))))
+       (.setPreferredSize (java.awt.Dimension. 
+                      	     (* 10 (inc w)) (* 10 (inc h))))))
+   .pack
+   (.setVisible true)))
+
+
+(let [w 40, h 40
+      grid (grid w h)
+      walls (maze grid)
+      labyrinth (reduce disj grid walls)
+      places (distinct (apply concat labyrinth))
+      theseus (rand-nth places)
+      minotaur (rand-nth places)
+      full-path #(conj (z/path %) (z/node %)) ; erratum 
+      path (->> theseus
+             (ariadne-zip labyrinth) 
+             (iterate z/next)
+             (filter #(= minotaur (second (z/node %)))) ; erratum: replaced first by second
+             first full-path rest)] ; erratum: replaced z/path by full-path
+  (draw w h walls path))
---
+++ Clojure/maze-clj/test/maze_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns maze-clj.core-test
+  (:require [clojure.test :refer :all]
+            [maze-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
