921fe553fad5c7cc56adec5ba2ca99584d3aeef6
CanftIn <wwc7033@gmail.com>
2020-07-16 18:01:28 +0800

    update

--- CPP/sqlite_test/sqlite_test.c
+++ CPP/sqlite_test/sqlite_test.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sqlite3.h>
 
-int main(int argc, char *argv[])
+static int callback(void *data, int argc, char **argv, char **azColName){
+   int i;
+   fprintf(stderr, "%s: ", (const char*)data);
+   for(i=0; i<argc; i++){
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
+
+int main(int argc, char* argv[])
 {
-    sqlite3 *db;
-    char *zErrMsg = 0;
-    int rc;
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   char *sql;
+   const char* data = "Callback function called";
 
-    rc = sqlite3_open("test.db", &db);
+   /* Open database */
+   rc = sqlite3_open("test.db", &db);
+   if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+   }else{
+      fprintf(stderr, "Opened database successfully\n");
+   }
 
-    if (rc)
-    {
-        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-        exit(0);
-    }
-    else
-    {
-        fprintf(stderr, "Opened database successfully\n");
-    }
-    sqlite3_close(db);
+   /* Create SQL statement */
+   sql = "SELECT * from COMPANY";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   }else{
+      fprintf(stdout, "Operation done successfully\n");
+   }
+   sqlite3_close(db);
+   return 0;
 }
