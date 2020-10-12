f61cb3561e9b9d6ce722424ac9d92adace8ce8dd
CanftIn <wwc7033@gmail.com>
2020-10-12 14:31:12 +0800

    commit

--- CPP/2020/boost_test/filesystem/filesystem_test.cc
+++ CPP/2020/boost_test/filesystem/filesystem_test.cc
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <boost/filesystem.hpp>
-
+#include "file_io.h"
 //using namespace boost::filesystem;
 
 int main() {
+  /*
   std::string _file_path = "../";
   std::string file_name = "file.txt";
   boost::filesystem::path file_path(_file_path);
@@ -12,5 +15,10 @@ int main() {
   std::cout << full_file_path.empty() << std::endl;
   std::cout << "system_complete: " << boost::filesystem::system_complete(full_file_path) << std::endl;
   std::cout << "generic_string: " << file_path.generic_string() << std::endl;
+  */
+  std::vector<std::string> file_vec;
+  FileIO::GetFileList("./", &file_vec);
+
+  for_each(file_vec.begin(), file_vec.end(), [](std::string it){ std::cout << it << std::endl; });
   return 0;
 }
