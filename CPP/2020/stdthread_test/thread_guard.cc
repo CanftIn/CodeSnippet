508b76ba90eecb4cab11a69aff3633502d9ad5d6
CanftIn <wwc7033@gmail.com>
2020-10-12 11:54:59 +0800

    commit

---
+++ CPP/2020/stdthread_test/thread_guard.cc
@@ -0,0 +1,30 @@
+#include <thread>
+
+class thread_guard {
+  std::thread& t;
+public:
+  explicit thread_guard(std::thread& t_) : t(t_) {}
+  ~thread_guard() {
+    if (t.joinable()) {
+      t.join();
+    }
+  }
+  thread_guard(const thread_guard&) = delete;
+  thread_guard& operator = (const thread_guard&) = delete;
+};
+
+void do_something(int& i) {
+  ++i;
+}
+
+void do_something_in_current_thread() {}
+
+struct func {
+  int& i;
+  func(int& i_) : i(i_) {}
+  void operator () () {
+    for (unsigned j = 0; j < 1000000; ++j) {
+      do_something(i);
+    }
+  }
+}
