4f0686f991362b91ef3893e9dca7da4cab0aa8cf
CanftIn <wwc7033@gmail.com>
2023-09-02 22:35:08 +0800

    update

--- cpp/jsonrpc/include/jsonrpc/rpc.hpp
+++ cpp/jsonrpc/include/jsonrpc/rpc.hpp
@@ -5,6 +5,7 @@
 #include <optional>
 
 #include "jsonrpc/call_type.hpp"
+#include "jsonrpc/incremental_buffers.hpp"
 #include "jsonrpc/json.hpp"
 #include "jsonrpc/utils.hpp"
 
@@ -15,10 +16,13 @@ namespace internal {
 template <typename... Args>
 constexpr bool positional_args_v = (!is_arg_v<Args> && ...);
 
+template <typename... Args>
+constexpr bool named_args_v = sizeof...(Args) > 0 && (is_arg_v<Args> && ...);
+
 using id_type = nlohmann::json;
 using native_type = nlohmann::json;
-using jsonrpc::internal::Expected;
-using jsonrpc::internal::Unexpected;
+using jsonrpc::internal::expected;
+using jsonrpc::internal::unexpected;
 
 /// 代表客户端的请求
 struct request {
@@ -37,9 +41,110 @@ struct response {
 
 /// jsonrpc增量解析器
 class increment_parser {
+ public:
+  expected<request, std::string> get_request() {
+    try_parse_object();
+    if (!parsed_) {
+      return unexpected{"no request parsed"};
+    }
+    auto object = std::move(*parsed_);
+    parsed_.reset();
+    return parse_request(std::move(object));
+  }
 
+  expected<response, std::string> get_response() {
+    try_parse_object();
+    if (!parsed_) {
+      return unexpected{"no response parsed"};
+    }
+    auto object = std::move(*parsed_);
+    parsed_.reset();
+    return parse_response(std::move(object));
+  }
 
-  
+  char* buffer() {  //
+    return incremental_buffers_.in_place_buffer();
+  }
+
+  std::size_t buffer_capacity() const {  //
+    return incremental_buffers_.in_place_buffer_capacity();
+  }
+
+  void buffer_consumed(std::size_t bytes) {  //
+    incremental_buffers_.in_place_buffer_consumed(bytes);
+  }
+
+  void reserve_buffer(std::size_t bytes) {  //
+    incremental_buffers_.reserve_in_place_buffer(bytes);
+  }
+
+ private:
+  void try_parse_object() {
+    if (parsed_) {
+      return;
+    }
+    auto buffer = incremental_buffers_.get_parsed_buffer();
+    if (buffer) {
+      parsed_ = nlohmann::json::parse(*buffer);
+    }
+  }
+
+  static expected<response, std::string> parse_response(nlohmann::json&& res) {
+    auto id_it = res.find("id");
+    auto result_it = res.find("result");
+    auto error_it = res.find("error");
+
+    if (id_it == end(res)) {
+      return unexpected{"missing id field"};
+    }
+    if (result_it == end(res) && error_it == end(res)) {
+      return unexpected{"missing error and result field"};
+    }
+
+    response parsed;
+    parsed.id = std::move(*id_it);
+    if (error_it != end(res)) {
+      parsed.error = std::move(*error_it);
+    }
+    if (result_it != end(res)) {
+      parsed.result = std::move(*result_it);
+    }
+    return {std::move(parsed)};
+  }
+
+  static expected<request, std::string> parse_request(nlohmann::json&& req) {
+    auto id_it = req.find("id");
+    auto method_it = req.find("method");
+    auto params_it = req.find("params");
+
+    if (method_it == end(req)) {
+      return unexpected{"missing method field"};
+    }
+    if (!method_it->is_string()) {
+      return unexpected{"method field is not a string"};
+    }
+
+    request parsed;
+    parsed.method = method_it->get<std::string>();
+    if (params_it == end(req) || params_it->is_null()) {
+      parsed.args = nlohmann::json::array();
+    } else if (!params_it->is_array() && !params_it->is_object()) {
+      return unexpected{"non-structured arguments are not supported"};
+    } else {
+      parsed.args = std::move(*params_it);
+    }
+
+    if (id_it == end(req) || id_it->is_null()) {
+      parsed.type = call_type::notification;
+    } else {
+      parsed.type = call_type::request;
+      parsed.id = std::move(*id_it);
+    }
+    return {std::move(parsed)};
+  }
+
+  std::optional<nlohmann::json> parsed_;
+  incremental_buffers incremental_buffers_;
 };
 
 }  // namespace internal
@@ -57,7 +162,205 @@ class Rpc {
 
   template <typename... Args>
   static auto serialize_notification(std::string_view method, Args&&... args)
-      -> std::enable_if_t<internal::positional_args_v<Args...>, std::string> {}
+      -> std::enable_if_t<internal::positional_args_v<Args...>, std::string> {
+    return nlohmann::json({
+                              {"jsonrpc", "2.0"},
+                              {"method", method},
+                              {"params", nlohmann::json::array({nlohmann::json(
+                                             std::forward<Args>(args))...})},
+                          })
+        .dump();
+  }
+
+  template <typename... Args>
+  static auto serialize_notification(std::string_view method, Args&&... args)
+      -> std::enable_if_t<internal::named_args_v<Args...>, std::string> {
+    return nlohmann::json({
+                              {"jsonrpc", "2.0"},
+                              {"method", method},
+                              {"params", {{args.name, args.value}...}},
+                          })
+        .dump();
+  }
+
+  template <typename... Args>
+  static auto serialize_notification(std::string_view, Args&&...)
+      -> std::enable_if_t<!internal::positional_args_v<Args...> &&
+                              !internal::named_args_v<Args...>,
+                          std::string> {
+    static_assert(
+        internal::positional_args_v<Args...> || internal::named_args_v<Args...>,
+        "JSON-RPC does not support mixed named and unnamed arguments");
+  }
+
+  template <typename... Args>
+  static auto serialize_request(const id_type& id, std::string_view method,
+                                Args&&... args)
+      -> std::enable_if_t<internal::positional_args_v<Args...>, std::string> {
+    return nlohmann::json({
+                              {"jsonrpc", "2.0"},
+                              {"method", method},
+                              {"params", nlohmann::json::array({nlohmann::json(
+                                             std::forward<Args>(args))...})},
+                              {"id", id},
+                          })
+        .dump();
+  }
+
+  template <typename... Args>
+  static auto serialize_request(const id_type& id, std::string_view method,
+                                Args&&... args)
+      -> std::enable_if_t<internal::named_args_v<Args...>, std::string> {
+    return nlohmann::json({
+                              {"jsonrpc", "2.0"},
+                              {"method", method},
+                              {"params", {{args.name, args.value}...}},
+                              {"id", id},
+                          })
+        .dump();
+  }
+
+  template <typename... Args>
+  static auto serialize_request(const id_type&, std::string_view, Args&&...)
+      -> std::enable_if_t<!internal::positional_args_v<Args...> &&
+                              !internal::named_args_v<Args...>,
+                          std::string> {
+    static_assert(
+        internal::positional_args_v<Args...> || internal::named_args_v<Args...>,
+        "JSON-RPC does not support mixed named and unnamed arguments");
+  }
+
+  static std::string serialize_response(const id_type& id) {
+    return serialize_response(id, nlohmann::json{});
+  }
+
+  template <typename T>
+  static std::string serialize_response(const id_type& id, T&& value) {
+    return nlohmann::json{
+        {"jsonrpc", "2.0"},
+        {"id", id},
+        {"result", std::forward<T>(value)},
+    }
+        .dump();
+  }
+
+  template <typename T>
+  static std::string serialize_error_response(const id_type& id, T&& value) {
+    return nlohmann::json{
+        {"jsonrpc", "2.0"},
+        {"id", id},
+        {"error",
+         [&]() {
+           nlohmann::json error = {
+               {"code", -32000},  // -32000 is an implementation-defined error
+               {"data", std::forward<T>(value)},
+           };
+           if (error["data"].is_string()) {
+             error["message"] = error["data"];
+           } else {
+             error["message"] = "unknown error";
+           }
+           return error;
+         }()},
+    }
+        .dump();
+  }
+
+  static boost::asio::const_buffer buffer(const std::string& buf) {
+    return boost::asio::const_buffer(buf.data(), buf.size());
+  }
+
+  template <typename T, typename F>
+  static internal::expected<T, std::string> extract_args(
+      const nlohmann::json& args, const args_specs<F>& specs) {
+    try {
+      if (args.is_array()) {
+        return convert_positional_args<T>(args, specs);
+      } else if (args.is_object()) {
+        return convert_named_args<T>(args, specs);
+      } else {
+        throw std::runtime_error{"arguments are not a structured type"};
+      }
+    } catch (const std::exception& exc) {
+      return internal::unexpected{std::string{"cannot convert arguments: "} +
+                                  exc.what()};
+    }
+  }
+
+ private:
+  template <typename T, typename F>
+  static constexpr T convert_positional_args(const nlohmann::json& array,
+                                             const args_specs<F>& specs) {
+    return convert_positional_args<T>(
+        array, specs, std::make_index_sequence<args_specs<F>::size()>());
+  }
+
+  template <typename T, typename F, std::size_t... Idxs>
+  static constexpr T convert_positional_args(const nlohmann::json& array,
+                                             const args_specs<F>& specs,
+                                             std::index_sequence<Idxs...>) {
+    if (!specs.options().allow_extra_arguments &&
+        array.size() > std::tuple_size_v<T>) {
+      throw std::runtime_error{"too many arguments"};
+    }
+    return {[&]() {
+      if (Idxs < array.size()) {
+        try {
+          return array.at(Idxs).get<std::tuple_element_t<Idxs, T>>();
+        } catch (const ::nlohmann::json::type_error&) {
+          throw std::runtime_error{"invalid type for argument " +
+                                   specs.template get<Idxs>().name()};
+        }
+      }
+      if (const auto& value = specs.template get<Idxs>().default_value()) {
+        return *value;
+      }
+      throw std::runtime_error{"no value for argument " +
+                               specs.template get<Idxs>().name()};
+    }()...};
+  }
+
+  template <typename T, typename F>
+  static T convert_named_args(const nlohmann::json& args,
+                              const args_specs<F>& specs) {
+    return convert_named_args<T>(
+        args, specs, std::make_index_sequence<args_specs<F>::size()>{});
+  }
+
+  template <typename T, typename F, std::size_t... Idxs>
+  static T convert_named_args(const nlohmann::json& args,
+                              const args_specs<F>& specs,
+                              std::index_sequence<Idxs...>) {
+    if (!specs.options().allow_extra_arguments) {
+      const std::array<const std::string*, sizeof...(Idxs)>
+          available_arguments = {&specs.template get<Idxs>().name()...};
+      for (auto it = args.begin(); it != args.end(); ++it) {
+        auto arg_it = std::find_if(
+            available_arguments.begin(), available_arguments.end(),
+            [&](const std::string* arg) { return *arg == it.key(); });
+        if (arg_it == available_arguments.end()) {
+          throw std::runtime_error{"unexpected argument " + it.key()};
+        }
+      }
+    }
+
+    return T{[&]() {
+      auto it = args.find(specs.template get<Idxs>().name());
+      if (it != args.end()) {
+        try {
+          return it->template get<std::tuple_element_t<Idxs, T>>();
+        } catch (const ::nlohmann::json::type_error&) {
+          throw std::runtime_error{"invalid type for argument " +
+                                   specs.template get<Idxs>().name()};
+        }
+      }
+      if (const auto& value = specs.template get<Idxs>().default_value()) {
+        return *value;
+      }
+      throw std::runtime_error{"no value for argument " +
+                               specs.template get<Idxs>().name()};
+    }()...};
+  }
 };
 
 }  // namespace jsonrpc
--- cpp/jsonrpc/include/jsonrpc/utils.hpp
+++ cpp/jsonrpc/include/jsonrpc/utils.hpp
@@ -4,12 +4,151 @@
 #include <string>
 #include <string_view>
 #include <variant>
+#include <optional>
+#include <stdexcept>
+#include <sstream>
+#include <type_traits>
+#include <vector>
+
+#include <boost/asio.hpp>
+
+#define PACKIO_TRACE(...) (void)0
+#define PACKIO_DEBUG(...) (void)0
+#define PACKIO_INFO(...) (void)0
+#define PACKIO_WARN(...) (void)0
+#define PACKIO_ERROR(...) (void)0
 
 namespace jsonrpc {
 
+namespace internal {
+
+template <typename, typename = void>
+struct func_traits : std::false_type {
+};
+
+template <typename T>
+struct func_traits<T, std::void_t<decltype(&std::decay_t<T>::operator())>>
+    : func_traits<decltype(&std::decay_t<T>::operator())> {
+};
+
+template <typename C, typename R, typename... Args>
+struct func_traits<R (C::*)(Args...)> : func_traits<R (*)(Args...)> {
+};
+
+template <typename C, typename R, typename... Args>
+struct func_traits<R (C::*)(Args...) const> : func_traits<R (*)(Args...)> {
+};
+
+template <typename R, typename... Args>
+struct func_traits<R (*)(Args...)> : std::true_type {
+    using result_type = R;
+    using args_type = std::tuple<Args...>;
+    static constexpr auto args_count = std::tuple_size_v<args_type>;
+};
+
+template <typename T>
+constexpr bool func_traits_v = func_traits<T>::value;
+
+template <typename>
+struct is_awaitable : std::false_type {
+};
+
+#if defined(PACKIO_HAS_CO_AWAIT)
+template <typename... Args>
+struct is_awaitable<net::awaitable<Args...>> : std::true_type {
+};
+#endif // defined(PACKIO_HAS_CO_AWAIT)
+
+template <typename, typename = void>
+struct is_coroutine : std::false_type {
+};
+
+template <typename T>
+struct is_coroutine<T, std::enable_if_t<func_traits_v<T>>>
+    : is_awaitable<typename func_traits<T>::result_type> {
+};
+
+template <typename T>
+constexpr bool is_coroutine_v = is_coroutine<T>::value;
+
+template <typename T, typename = void>
+struct is_tuple : std::false_type {
+};
+
+template <typename T>
+struct is_tuple<T, std::void_t<decltype(std::tuple_size<std::decay_t<T>>::value)>>
+    : std::true_type {
+};
+
+template <typename T>
+constexpr auto is_tuple_v = is_tuple<T>::value;
+
+template <typename T>
+struct left_shift_tuple;
+
+template <typename A, typename... Bs>
+struct left_shift_tuple<std::tuple<A, Bs...>> {
+    using type = std::tuple<Bs...>;
+};
+
+template <typename T>
+using left_shift_tuple_t = typename left_shift_tuple<T>::type;
+
+template <template <typename...> class Map, typename T>
+struct map_tuple;
+
+template <template <typename...> class Map, typename... Args>
+struct map_tuple<Map, std::tuple<Args...>> {
+    using type = std::tuple<Map<Args>...>;
+};
+
+template <template <typename...> class Map, typename T>
+using map_tuple_t = typename map_tuple<Map, T>::type;
+
+template <typename T>
+using decay_tuple_t = map_tuple_t<std::decay_t, T>;
+
+template <typename... Args>
+struct always_false : std::false_type {
+};
+
+template <typename... Args>
+constexpr auto always_false_v = always_false<Args...>::value;
+
+template <typename T>
+void set_no_delay(T&)
+{
+}
+
+template <>
+inline void set_no_delay(boost::asio::ip::tcp::socket& socket)
+{
+    boost::system::error_code ec;
+    socket.set_option(boost::asio::ip::tcp::no_delay{true}, ec);
+    if (ec) {
+        PACKIO_WARN("error setting tcp nodelay option: {}", ec.message());
+    }
+}
+
+template <typename T>
+std::unique_ptr<std::decay_t<T>> to_unique_ptr(T&& value)
+{
+    return std::make_unique<std::decay_t<T>>(std::forward<T>(value));
+}
+
+template <typename Executor, typename Obj>
+auto bind_executor(Executor&& executor, Obj&& obj)
+{
+    return boost::asio::bind_executor(
+        boost::asio::any_io_executor(std::forward<Executor>(executor)),
+        std::forward<Obj>(obj));
+}
+
+} // internal
+
 ///-----------------------------------------------------------------------------
 /// Arg
-class Arg {
+class arg {
  public:
   template <typename T>
   struct with_value {
@@ -17,7 +156,7 @@ class Arg {
     T value;
   };
 
-  explicit constexpr Arg(std::string_view name) : name_(name) {}
+  explicit constexpr arg(std::string_view name) : name_(name) {}
 
   std::string_view name() const { return name_; }
 
@@ -34,7 +173,7 @@ template <typename T>
 struct is_arg_impl : std::false_type {};
 
 template <typename T>
-struct is_arg_impl<Arg::with_value<T>> : std::true_type {};
+struct is_arg_impl<arg::with_value<T>> : std::true_type {};
 
 template <typename T>
 struct is_arg : is_arg_impl<std::decay_t<T>> {};
@@ -44,18 +183,150 @@ constexpr bool is_arg_v = is_arg<T>::value;
 
 namespace arg_literals {
 
-constexpr Arg operator"" _arg(const char* str, std::size_t) { return Arg{str}; }
+constexpr arg operator"" _arg(const char* str, std::size_t) { return arg{str}; }
 
 }  // namespace arg_literals
+
+///-----------------------------------------------------------------------------
+/// handler
+
+//! The completion_handler class
+//! @tparam Rpc RPC protocol implementation
+//!
+//! First argument of @ref traits::AsyncProcedure "AsyncProcedure", the
+//! completion_handler is a callable used to notify the completion of an
+//! asynchronous procedure. You must only call @ref set_value or
+//! @ref set_error once.
+template <typename Rpc>
+class completion_handler {
+public:
+    using id_type = typename Rpc::id_type;
+    using response_buffer_type =
+        decltype(Rpc::serialize_response(std::declval<id_type>()));
+    using function_type = std::function<void(response_buffer_type&&)>;
+
+    template <typename F>
+    completion_handler(const id_type& id, F&& handler)
+        : id_(id), handler_(std::forward<F>(handler))
+    {
+    }
+
+    //! The destructor will notify an error if the completion_handler has not been used
+    ~completion_handler()
+    {
+        if (handler_) {
+            set_error("call finished with no result");
+        }
+    }
+
+    completion_handler(const completion_handler&) = delete;
+    completion_handler& operator=(const completion_handler&) = delete;
+
+    //! Move constructor
+    completion_handler(completion_handler&& other)
+        : id_(other.id_), handler_(std::move(other.handler_))
+    {
+        other.handler_ = nullptr;
+    }
+
+    //! Move assignment operator
+    completion_handler& operator=(completion_handler&& other)
+    {
+        if (handler_) {
+            set_error("call finished with no result");
+        }
+        id_ = other.id_;
+        handler_ = std::move(other.handler_);
+        other.handler_ = nullptr;
+        return *this;
+    }
+
+    //! Notify successful completion of the procedure and set the return value
+    //! @param return_value The value that the procedure will return to the client
+    template <typename T>
+    void set_value(T&& return_value)
+    {
+        complete(Rpc::serialize_response(id_, std::forward<T>(return_value)));
+    }
+
+    //! @overload
+    void set_value() { complete(Rpc::serialize_response(id_)); }
+
+    //! Notify erroneous completion of the procedure with an associated error
+    //! @param error_value Error value
+    template <typename T>
+    void set_error(T&& error_value)
+    {
+        complete(Rpc::serialize_error_response(id_, std::forward<T>(error_value)));
+    }
+
+    //! @overload
+    void set_error()
+    {
+        complete(Rpc::serialize_error_response(id_, "unknown error"));
+    }
+
+    //! Same as @ref set_value
+    template <typename T>
+    void operator()(T&& return_value)
+    {
+        set_value(std::forward<T>(return_value));
+    }
+
+    //! Same as @ref set_value
+    void operator()() { set_value(); }
+
+private:
+    void complete(response_buffer_type&& buffer)
+    {
+        handler_(std::move(buffer));
+        handler_ = nullptr;
+    }
+
+    id_type id_;
+    function_type handler_;
+};
+
+template <typename>
+struct is_completion_handler : std::false_type {
+};
+
+template <typename T>
+struct is_completion_handler<completion_handler<T>> : std::true_type {
+};
+
+template <typename T>
+constexpr auto is_completion_handler_v = is_completion_handler<T>::value;
+
+template <typename...>
+struct is_async_procedure_args;
+
+template <>
+struct is_async_procedure_args<std::tuple<>> : std::false_type {
+};
+
+template <typename T0, typename... Ts>
+struct is_async_procedure_args<std::tuple<T0, Ts...>>
+    : is_completion_handler<T0> {
+};
+
+template <typename F>
+struct is_async_procedure
+    : is_async_procedure_args<typename internal::func_traits<F>::args_type> {
+};
+
+template <typename T>
+constexpr auto is_async_procedure_v = is_async_procedure<T>::value;
+
 
 ///-----------------------------------------------------------------------------
 /// Expect
 namespace internal {
 
 template <typename Error>
-struct Unexpected {
+struct unexpected {
  public:
-  explicit constexpr Unexpected(Error error) : error_(std::move(error)) {}
+  explicit constexpr unexpected(Error error) : error_(std::move(error)) {}
 
   Error& error() { return error_; }
 
@@ -64,13 +335,13 @@ struct Unexpected {
 };
 
 template <typename T, typename Error>
-class Expected {
+class expected {
  public:
   template <typename G>
-  Expected(Unexpected<G> unexpected)
+  expected(unexpected<G> unexpected)
       : data_(Error(std::move(unexpected.error()))) {}
 
-  Expected(T value) : data_(std::move(value)) {}
+  expected(T value) : data_(std::move(value)) {}
 
   Error& error() { return std::get<Error>(data_); }
   T& value() { return std::get<T>(data_); }
@@ -85,6 +356,170 @@ class Expected {
 
 }  // namespace internal
 
+///-----------------------------------------------------------------------------
+/// args_spec
+namespace internal {
+
+template <typename DefaultType>
+class arg_spec {
+public:
+    explicit arg_spec(std::string name) : name_(std::move(name)) {}
+    explicit arg_spec(const arg& arg) : name_(arg.name()) {}
+    explicit arg_spec(arg::with_value<DefaultType> arg)
+        : name_(std::move(arg.name)), default_value_(std::move(arg.value))
+    {
+    }
+
+    const std::string& name() const { return name_; }
+    const std::optional<DefaultType>& default_value() const
+    {
+        return default_value_;
+    }
+
+private:
+    std::string name_;
+    std::optional<DefaultType> default_value_;
+};
+
+template <typename F>
+using arg_specs_tuple_for_sync_procedure_t =
+    map_tuple_t<arg_spec, decay_tuple_t<typename func_traits<F>::args_type>>;
+
+template <typename F>
+using arg_specs_tuple_for_async_procedure_t =
+    left_shift_tuple_t<arg_specs_tuple_for_sync_procedure_t<F>>;
+
+template <typename, bool>
+struct arg_specs_tuple_for;
+
+template <typename F>
+struct arg_specs_tuple_for<F, true> {
+    using type = arg_specs_tuple_for_async_procedure_t<F>;
+};
+
+template <typename F>
+struct arg_specs_tuple_for<F, false> {
+    using type = arg_specs_tuple_for_sync_procedure_t<F>;
+};
+
+template <typename F>
+using arg_specs_tuple_for_t =
+    typename arg_specs_tuple_for<F, is_async_procedure_v<F>>::type;
+
+template <typename T>
+struct args_specs_maker;
+
+template <typename... Specs>
+struct args_specs_maker<std::tuple<Specs...>> {
+    template <typename... Args>
+    static std::tuple<Specs...> make(Args&&... args)
+    {
+        if constexpr (sizeof...(Args) == 0) {
+            // default arg specs are arguments called "0", "1", ... and no default value
+            return iota(std::make_index_sequence<sizeof...(Specs)>());
+        }
+        else {
+            static_assert(
+                sizeof...(Args) == sizeof...(Specs),
+                "arguments specification must either match the number of "
+                "arguments or be empty");
+            return {Specs{std::forward<Args>(args)}...};
+        }
+    }
+
+    template <std::size_t... Idxs>
+    static std::tuple<Specs...> iota(std::index_sequence<Idxs...>)
+    {
+        return {Specs{std::to_string(Idxs)}...};
+    }
+};
+
+//! Options available for the argument specifications
+//!
+//! You are never expected to construct this structure yourself
+//! but rather construct them by combining options such as
+//! @ref packio::allow_extra_arguments "allow_extra_arguments"
+//! with operator|
+struct args_specs_options {
+    bool allow_extra_arguments = false;
+
+    constexpr args_specs_options operator|(const args_specs_options& other)
+    {
+        auto ret = *this;
+        ret.allow_extra_arguments |= other.allow_extra_arguments;
+        return ret;
+    }
+};
+
+template <typename SpecsTuple>
+class args_specs {
+public:
+    args_specs() : args_specs(args_specs_options{}){};
+
+    template <
+        typename T,
+        typename... Args,
+        typename = std::enable_if_t<!std::is_same_v<std::decay_t<T>, args_specs_options>>>
+    args_specs(T&& arg0, Args&&... args)
+        : args_specs(
+            args_specs_options{},
+            std::forward<T>(arg0),
+            std::forward<Args>(args)...)
+    {
+    }
+
+    template <typename... Args>
+    args_specs(args_specs_options opts, Args&&... args)
+        : specs_{args_specs_maker<SpecsTuple>::make(std::forward<Args>(args)...)},
+          opts_{std::move(opts)}
+    {
+    }
+
+    constexpr const args_specs_options& options() const { return opts_; }
+
+    template <std::size_t I>
+    constexpr decltype(auto) get() const
+    {
+        return std::get<I>(specs_);
+    }
+
+    static constexpr std::size_t size()
+    {
+        return std::tuple_size_v<SpecsTuple>;
+    }
+
+private:
+    SpecsTuple specs_;
+    args_specs_options opts_{};
+};
+} // internal
+
+//! Procedure arguments specifications
+//! @tparam The procedure
+//!
+//! CLass that describes the arguments of the procedure
+//! They each have a name and optionally a default value
+//! This is a tuple-like class where each element can be
+//! constructured from:
+//! - a string, defining the name of the argument
+//! - a @ref arg, defining the name of the argument
+//! - a @ref arg::with_value, defining the name of the argyment
+//!   and its default value
+//! Optionally, accepts
+//! @ref packio::internal::args_specs_options "args_specs_options"
+//! as first argument to customize the behavior of argument handling
+template <typename Procedure>
+class args_specs
+    // Using the real implementation as the base class reduces
+    // the number of templates instanciation
+    : public internal::args_specs<internal::arg_specs_tuple_for_t<Procedure>> {
+public:
+    using base = internal::args_specs<internal::arg_specs_tuple_for_t<Procedure>>;
+    using base::base;
+};
+
+//! Option to allo extra arguments, ignoring them
+constexpr auto allow_extra_arguments = internal::args_specs_options{true};
 }  // namespace jsonrpc
 
 #endif  // JSONRPC_UTILS_HPP
