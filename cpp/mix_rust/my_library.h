8825a28ebc8f0fdb893717dae906249c96687b8b
CanftIn <wwc7033@gmail.com>
2023-11-14 18:14:00 +0800

    add rust and cpp mix test

---
+++ cpp/mix_rust/my_library.h
@@ -0,0 +1,14 @@
+#ifndef MY_LIBRARY_H
+#define MY_LIBRARY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void say_hello();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MY_LIBRARY_H
---
+++ cpp/mix_rust/test.cpp
@@ -0,0 +1,6 @@
+#include "my_library.h"
+
+int main() {
+  say_hello();
+  return 0;
+}
