c15f69a8edf19542919cc5df8741863854270994
CanftIn <wwc7033@gmail.com>
2023-12-02 20:28:13 +0800

    add python fpy

---
+++ python/fpy/function.py
@@ -0,0 +1,71 @@
+import inspect
+import collections.abc as cabc
+
+from composable import Composable
+from transparent import Transparent
+
+from typing import Callable
+
+import sys
+import traceback
+
+class SignatureMismatchError(Exception):
+    def __init__(self, e):
+        self.e = e
+
+
+class NotEnoughArgsError(Exception):
+    def __init__(self, expect, got):
+        self.expect = expect
+        self.got = got
+
+
+class func(Composable, Transparent):
+    fn: Callable = None
+
+    def __init__(self, f, *args, **kwargs):
+        if isinstance(f, func):
+            self.fn = f.fn
+            self.args = (*f.args, *args)
+            self.kwargs = {**f.kwargs, **kwargs}
+            self.sig = f.sig
+        elif isinstance(f, cabc.Callable):
+            self.fn = f
+            self.args = args
+            self.kwargs = kwargs
+            self.sig = inspect.signature(f)
+        else:
+            raise TypeError(f"{f} is not callable")
+
+    def __underlying__(self):
+        return self.fn
+
+    def __repr__(self):
+        return repr(self.fn)
+
+    def __call__(self, *args, **kwargs):
+        _args = (*self.args, *args)
+        _kwargs = {**self.kwargs, **kwargs}
+        try:
+            self.sig.bind(*_args, **_kwargs)
+            return self.fn(*_args, **_kwargs)
+        except TypeError as e:
+            # print(f"TypeError: {e}", file=sys.stderr)
+            # tb = sys.exc_info()[2]
+            # print(f"{tb.tb_frame.f_code.co_filename = }, {tb.tb_lineno = }")
+            # traceback.print_tb(tb)
+            try:
+                self.sig.bind_partial(*_args, **_kwargs)
+                return func(self, *args, **kwargs)
+            except TypeError as e:
+                raise SignatureMismatchError(e)
+        except NotEnoughArgsError:
+            return func(self, *args, **kwargs)
+
+    def __compose__(self, other):
+        if isinstance(other, func):
+            return self.__class__(lambda *args, **kwargs: other(self(*args, **kwargs)))
+        if isinstance(other, cabc.Callable):
+            fn = func(other)
+            return self.__class__(lambda *args, **kwargs: fn(self(*args, **kwargs)))
+        raise TypeError(f"Cannot compose function with {type(other)}")
---
+++ python/fpy/test_composable.py
@@ -0,0 +1,34 @@
+from function import func, SignatureMismatchError, NotEnoughArgsError
+
+import unittest
+
+
+def add1(x):
+    return x + 1
+
+
+def mul2(x):
+    return x * 2
+
+
+def pos(x):
+    return x > 0
+
+
+def odd(x):
+    return x % 2 != 0
+
+
+def add(a, b):
+    return a + b
+
+
+class TestCompsable(unittest.TestCase):
+    def testFunc(self):
+        f1 = func(add1)
+        f2 = func(mul2)
+        self.assertEqual(4, (f1 ^ f2)(1))
+        self.assertEqual(3, (f2 ^ f1)(1))
+
+
+unittest.main()
